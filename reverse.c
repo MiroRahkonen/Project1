a2fce49d93572c6d58c77d11756db87f3bd360e3
Miro Rahkonen <rahkonenmiro000@gmail.com>
2022-03-08 05:46:17 +0200

    Program about finished

--- reverse.c
+++ reverse.c
@@ -9,18 +9,18 @@ typedef struct list {
 }LIST;
 
 
-LIST *addtolist(LIST *listStart,char line[100]){
+LIST *addtolist(LIST *listStart,char line[100]){	/*This function adds the line to the end of the list*/
 	LIST *newNode,*ptrList;
-    if((newNode = (LIST*) malloc(sizeof(LIST))) == NULL){
-		fprintf(stderr,"malloc failed");
+    if((newNode = (LIST*) malloc(sizeof(LIST))) == NULL){	/*allocating space for*/
+		fprintf(stderr,"malloc failed");					/*the next node in the list*/
 		exit(1);
 	}
 	strcpy(newNode->line,line);
 	newNode->next = NULL;
 
-	if(listStart == NULL){
-		listStart = newNode;
-		listStart->previous = NULL;
+	if(listStart == NULL){			/*Here we either make the new node the start of the list*/
+		listStart = newNode;		/*or find the last place in the list and add it as the*/
+		listStart->previous = NULL;	/*last one*/
 	}
 	else{
 		ptrList = listStart;
@@ -33,17 +33,17 @@ LIST *addtolist(LIST *listStart,char line[100]){
     return listStart;
 }
 
-void printFromFile(LIST *listStart){
+void printFromFile(LIST *listStart){	/*This function prints all the lines from the linked list*/
 	LIST *ptrList;
 	ptrList = listStart;
-	while(ptrList->next != NULL){
+	while(ptrList->next != NULL){		/*Here we go to the end of the list*/
 		ptrList = ptrList->next;
 	}
-	while(ptrList->previous != NULL){
-		fprintf(stdout,ptrList->line);
+	while(ptrList->previous != NULL){	/*We start printing from the last one*/
+		fprintf(stdout,ptrList->line);	/*and go towards the start of the list*/
 		ptrList = ptrList->previous;
 	}
-	printf("%s",ptrList->line);
+	fprintf(stdout,ptrList->line);		/*We print the last one remaining*/
 	return;
 }
 
@@ -52,12 +52,8 @@ int main(int argc, char *argv[]){
 	FILE *inputFile;
 	char inputfilename[100];
 	char line[1000];
-	if(argc < 2){
-		fprintf(stdout ,"Not enough arguments, usage: reverse <input>\n");
-		exit(1);
-	}
-	else if(argc > 3){
-		fprintf(stdout,"Too many arguments, usage: reverse <input>\n");
+	if(argc != 2){
+		fprintf(stdout ,"usage: reverse <input>\n");
 		exit(1);
 	}
 	else{
@@ -65,14 +61,14 @@ int main(int argc, char *argv[]){
 			fprintf(stderr,"malloc failed");
 			exit(1);
 		}
-		strcpy(inputfilename,argv[1]);
+		strcpy(inputfilename,argv[1]);	/*Copying the second argument as the input file name*/
     	inputFile = fopen(inputfilename,"r");
 		if(inputFile == NULL){
         	fprintf(stderr,"error: cannot open file '%s'\n",inputfilename);
         	exit(1);
 		}
-		while(fgets(line,1000,inputFile) != NULL){
-			listStart = addtolist(listStart,line);
+		while(fgets(line,1000,inputFile) != NULL){	/*Reading lines from the file and sending them*/
+			listStart = addtolist(listStart,line);	/*to the function that adds them to the linked list*/
     	}
 		fclose(inputFile);
 		printFromFile(listStart);
