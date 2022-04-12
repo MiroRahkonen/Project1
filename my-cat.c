b8280f870d08189c19550a52686265f43d1411f7
MiroRahkonen <80097068+MiroRahkonen@users.noreply.github.com>
2022-04-12 18:07:36 +0300

    Add files via upload

---
+++ my-cat.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main(int argc,char *argv[]){
+    char *filename,line[1000];
+    int i,test;
+    FILE *file;
+
+    if(argc < 2){
+        fprintf(stdout,"my-cat: [file ...]\n");
+        exit(0);
+    }
+    else{
+        for(i=1; i<argc; i++){
+            filename = argv[i];
+            file = fopen(filename,"r");
+            if(file == NULL){
+                fprintf(stderr,"my-cat: cannot open file\n");
+                exit(1);
+            }
+            test = fgetc(file);
+            if(test == EOF){    /*testing if file is empty*/
+                fclose(file);
+            }
+            else{
+                rewind(file);
+                while(fgets(line,1000,file) != NULL){
+                    fprintf(stdout,"%s",line);
+                }
+            }
+        }
+    }
+    fclose(file);
+    return 0;
+}
